8fff6c76d69ac2c23ef92b90f3df082d89d7d3cc
dieptv1999 <diep.tv1999@gmail.com>
2023-09-15 22:08:20 +0700

    .

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
                     <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b
                     from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit
                     lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:px-4 lg:pb-2 lg:pt-2.5 lg:dark:bg-zinc-800/30
-                    text-xl">
+                    text-xl text-black">
                         Tra cứu phạt nguội toàn quốc
                     </p>
                     {/*<div*/}
