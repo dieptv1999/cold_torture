20d1f5bef138c21ec7d0a8237734f0cd51632e60
dieptv1999 <diep.tv1999@gmail.com>
2023-09-16 10:40:02 +0700

    .

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Head from "next/head";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -23,6 +24,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
